577eb7b5af5251e38eeabb416baf9472e995e03a
skader-collab <kamarasekous22@gmail.com>
2025-12-02 09:19:05 +0000

    Update settings.py

--- Cipher/settings.py
+++ Cipher/settings.py
@@ -158,7 +158,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://cyberguardian.up.railway.app",
+    "https://*.railway.app",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
